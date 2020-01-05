ed5a11685cd3d23eb29cf9b2204e89f8dae8324d
salma1601 <salmabougacha@hotmail.com>
2020-01-05 22:31:49 +0100

    fix N4 out_file bug

--- sammba/preprocessing/bias_correction.py
+++ sammba/preprocessing/bias_correction.py
@@ -60,7 +60,7 @@ def ants_n4(in_file, write_dir=None, caching=False,
         output_image=output_image)
 
     if copy_geometry:
-        out_copy_geom = copy_geom(dest_file=out_bias_correct.outputs.out_file,
+        out_copy_geom = copy_geom(dest_file=out_bias_correct.outputs.output_image,
                                   in_file=in_file)
         out_copy = copy(
             in_file=out_copy_geom.outputs.output_image,
